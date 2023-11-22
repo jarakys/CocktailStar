82a1182b9c4fa0abf585026daf4bca2fd8c8a61b
KyryloChernovUptime <kyrylo.chernov@uptimedevelopment.pl>
2023-11-22 16:05:36 +0200

    Initial Commit

---
+++ CocktailStar/CocktailStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailStarApp.swift
+//  CocktailStar
+//
+//  Created by Kyrylo Chernov on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CocktailStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
